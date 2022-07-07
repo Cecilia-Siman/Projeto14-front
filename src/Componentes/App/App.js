e81f7fd7ba96863a76cc2f027c012a165b70bc99
Cecilia Salema <ceciliasiman15@gmail.com>
2022-07-07 20:28:13 -0300

    tela login

--- src/Componentes/App/App.js
+++ src/Componentes/App/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from '../Cadastro/Cadastro.js';
 import Produtos from '../Produtos/Produtos.js';
+import Login from '../Login/Login.js';
 
 function App() {
 
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Login />} />
                 <Route path='/cadastro' element={<Cadastro />} />
                 <Route path='/produtos' element={<Produtos />} />
             </Routes>
