52ca89931f9a89f1a7a3d78ba6720d6fb08e4ef4
Cecilia Salema <ceciliasiman15@gmail.com>
2022-07-07 19:46:47 -0300

    tela login

--- src/Componentes/App/App.js
+++ src/Componentes/App/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from '../Cadastro/Cadastro.js';
+import Login from '../Login/Login.js';
 
 function App() {
 
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Login />} />
                 <Route path='/cadastro' element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
---
+++ src/Componentes/Login/Login-style.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+position: absolute;
+padding: 0 20px;
+box-sizing: border-box;
+
+form{ 
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    z-index: 1;
+}
+
+button{
+    height: 30px;
+    background-color: #6495ed;
+    border: 0;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    cursor: pointer;
+}
+
+img {
+    position: fixed;
+    height: 100%;
+    left: 0;
+    background-color: red;
+    opacity: .3;
+    object-fit: cover;
+}
+h2 {
+        width: 100%;
+        text-align: center;
+        color:#FFFFFF;
+        z-index: 1;
+        cursor: pointer;
+    }
+
+h2:hover{
+    text-decoration: underline;
+}
+`
+
+export const Cabecalho = styled.div`
+margin-bottom:50px ;
+z-index: 1;
+
+h1{
+    display: flex;
+    justify-content: center;
+    font-size: 50px;
+    text-align: center;
+    font-family: 'Orbitron', sans-serif;
+    font-weight: 500;
+}
+`
