b4ac71ed426ea380ab4554c5ee48aff5b09ee3e9
MattGueler <mateus.gueler@gmail.com>
2022-07-09 19:07:45 -0300

    Página de perfil

--- src/Componentes/App/App.js
+++ src/Componentes/App/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from '../Cadastro/Cadastro.js';
 import Produtos from '../Produtos/Produtos.js';
 import Login from '../Login/Login.js';
+import Perfil from '../Perfil/Perfil.js';
 import TokenContext from '../Context/TokenContext.js';
 import { useState } from 'react';
 
@@ -14,6 +15,7 @@ function App() {
             <TokenContext.Provider value={{ token, setToken }}>
                 <Routes>
                     <Route path='/' element={<Login />} />
+                    <Route path='/perfil' element={<Perfil />} />
                     <Route path='/cadastro' element={<Cadastro />} />
                     <Route path='/produtos' element={<Produtos />} />
                 </Routes>
