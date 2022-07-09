a4486d210df76b1105f9434d3071567873982b7e
MattGueler <mateus.gueler@gmail.com>
2022-07-09 15:10:30 -0300

    Corrige a escrita de confirmacao

--- src/Componentes/Cadastro/Cadastro.js
+++ src/Componentes/Cadastro/Cadastro.js
@@ -10,21 +10,21 @@ function Cadastro() {
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
-    const [confmacao, setConfirmacao] = useState('')
+    const [confirmacao, setConfirmacao] = useState('')
 
     let navigate = useNavigate();
 
     function SignUp(event) {
         event.preventDefault();
 
-        const senhasDiferentes = senha !== confmacao
+        const senhasDiferentes = senha !== confirmacao
         const senhaPequena = senha.length < 6
 
         const body = {
             name: nome,
             email,
             password: senha,
-            passwordConfirm: confmacao
+            passwordConfirm: confirmacao
         }
 
         if (senhasDiferentes || senhaPequena) {
@@ -52,7 +52,7 @@ function Cadastro() {
                 <Input placeholder="Nome" type='text' onChange={(e) => { setNome(e.target.value) }} value={nome} />
                 <Input placeholder="Email" type='email' onChange={(e) => { setEmail(e.target.value) }} value={email} />
                 <Input placeholder="Senha" type='password' onChange={(e) => { setSenha(e.target.value) }} value={senha} />
-                <Input placeholder="Confirmar senha" type='password' onChange={(e) => { setConfirmacao(e.target.value) }} value={confmacao} />
+                <Input placeholder="Confirmar senha" type='password' onChange={(e) => { setConfirmacao(e.target.value) }} value={confirmacao} />
                 <button>Cadastrar</button>
             </form>
             <h2 onClick={() => navigate('/')}>Já sou cadastrado!!</h2>
